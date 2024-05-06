efb94f2848917ffaa0cbc0d05b5ee461367bf4b3
Qingyu Yu <yqy1998@gmail.com>
2024-05-05 23:12:44 -0700

    delete all frontend files and add them into the frontend folder

--- atom.ts
+++
@@ -1,4 +0,0 @@
-import { atom } from "jotai";
-import { VideoPostItem } from "./type";
-
-export const VideoItemsListAtom = atom<VideoPostItem[]>([]);
--- constants.ts
+++
@@ -1,6 +0,0 @@
-export const colors = {
-  primary: "#ff5722",
-  mainBg: "#f6f6f6",
-  mainText: "#231F20", // darker black
-  secondText: "#999693", // lighter black
-};
